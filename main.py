89411924b6109937cea93c3ab89a74f087b3317c
ApoloKn <polosilverajainer@hotmail.com>
2026-02-25 08:56:56 -0500

    Guardo cambios antes de actualizar

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+def main():
+    print("-"*10 + "Welcome to the calculator" + "-"*10)
+    print("""
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    4. Division
+          
+    5. Exit""")
 
+    print("-"*45)
+
+Cont = 0 
+while Cont != 5:
+    main()
+    Cont = int(input("Enter your option: "))
+    if Cont == 1:
+        Sum()
+    elif Cont == 2:
+        Rest()
+    elif Cont == 3:
+        Mul()
+    elif Cont == 4:
+        Div()
+    elif Cont == 5:
+        break
+    else:
+        print("!¡!¡!¡!¡ERROR!¡!¡!¡!¡")
+    
